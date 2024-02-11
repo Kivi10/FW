d3af604c7c9e4067b26a3acac0368a05b27a6640
Hamzat <hgeroev11@mail.com>
2024-02-11 15:46:25 +0300

    Добавил параметры

--- Laptop.java
+++ Laptop.java
@@ -1,5 +1,16 @@
 package FW;
 
+import Sem6.Cat.Cat;
+
 public class Laptop {
-    
+    String name;
+    int ozu;
+    int hdd;
+    String os;
+    String color;
+
+    public String toString() {
+        return "name: " + name + "\n" +"ОЗУ: " + ozu + "\n" + "Объем памяти: " + hdd + "\n" + 
+        "Операционная система: " + os + "\n" + "Цвет: " + color + "\n";
+    }
 }
